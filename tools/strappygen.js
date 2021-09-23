617a70636701ec2a5c3696c58b026ff368d1e4c6
Noah Levenson <noahlevenson@gmail.com>
2021-09-23 12:46:24 -0400

    output config to parent dir; fail on attempted overwrite

--- tools/strappygen.js
+++ tools/strappygen.js
@@ -8,7 +8,9 @@ const { Fid } = require("../libfood/src/fid/fid.js");
 const { Fid_pub } = require("../libfood/src/fid/fid_pub.js");
 const { Flog } = require("../libfood/src/flog/flog.js");
 
+const OUTPUT_PATH = "../";
 const OUTPUT_FILENAME = "strappy.secret.json";
+const CONFIG_PATH = `${OUTPUT_PATH}${OUTPUT_FILENAME}`;
 
 const proto = {
   "PUBLIC_HOST": null,
@@ -33,6 +35,13 @@ function ask(rl, q) {
 }
 
 (async () => {
+  if (fs.existsSync(CONFIG_PATH)) {
+    Flog.log(`[STRAPPYGEN] ${CONFIG_PATH} already exists! You must delete your ` + 
+      `existing configuration before generating a new one.`);
+    Flog.log(`[STRAPPYGEN] Exiting...`);
+    return;
+  }
+
   Flog.log(`[STRAPPYGEN] Welcome to strappygen, protocol vers ${cfg.PROTOCOL_VERSION}, ` + 
     `${cfg.FID_POW_ZERO_BITS}-bit POW`);
 
@@ -66,7 +75,7 @@ function ask(rl, q) {
   proto.ENCRYPTED_PRIVKEY = pair.privateKey;
   proto.NONCE = pow_cert.nonce;
 
-  Flog.log(`[STRAPPYGEN] Writing to ${OUTPUT_FILENAME}...`);
-  fs.writeFileSync(OUTPUT_FILENAME, JSON.stringify(proto, null, 2), {encoding: "utf8"});
+  Flog.log(`[STRAPPYGEN] Writing to ${CONFIG_PATH}...`);
+  fs.writeFileSync(CONFIG_PATH, JSON.stringify(proto, null, 2), {encoding: "utf8"});
   Flog.log(`[STRAPPYGEN] Done!`);
 })();
